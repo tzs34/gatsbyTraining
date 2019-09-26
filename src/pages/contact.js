e293ee49730316db5d195bd3b548fbd166e73724
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-09-26 20:53:01 +0100

    Added contact page form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,50 @@
 import React from 'react'
+import Layout from '../components/layout/layout'
+import GlobalStyle from '../components/styled/global-style'
+import styles from '../css/contact.module.css'
+export default () => (
+  <>
+    <GlobalStyle />
+    <Layout>
+      <div id={styles.formmain}>
+        <div id={styles.formdiv}>
+          <form className={styles.form} id={styles.form1}>
+            <p className={styles.name}>
+              <input
+                name="name"
+                type="text"
+                placeholder="Name"
+                id="name"
+                className={styles.feedbackinput}
+              />
+            </p>
 
-const Blog = () => <div>Blog</div>
+            <p className={styles.email}>
+              <input
+                name="email"
+                type="text"
+                id="email"
+                placeholder="Email"
+                className={styles.feedbackinput}
+              />
+            </p>
 
-export default Blog
+            <p className={styles.text}>
+              <textarea
+                name="text"
+                id="comment"
+                placeholder="Comment"
+                className={styles.feedbackinput}
+              ></textarea>
+            </p>
+
+            <div className={styles.submit}>
+              <input type="submit" value="SEND" className={styles.buttonblue} />
+              <div className={styles.ease}></div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </Layout>
+  </>
+)
--- src/pages/finance.js
+++ src/pages/finance.js
@@ -1,5 +1,34 @@
 import React from 'react'
-
-const Finance = () => <div>Finance</div>
-
-export default Finance
+import Layout from '../components/layout/layout'
+import GlobalStyle from '../components/styled/global-style'
+export default () => (
+  <>
+    <GlobalStyle />
+    <Layout>
+      <form>
+        <input
+          name="name"
+          placeholder="What is your name?"
+          class="name"
+          required
+        />
+        <input
+          name="emailaddress"
+          placeholder="What is your email?"
+          class="email"
+          type="email"
+          required
+        />
+        <textarea
+          rows="4"
+          cols="50"
+          name="subject"
+          placeholder="Please enter your message"
+          class="message"
+          required
+        ></textarea>
+        <input name="submit" class="btn" type="submit" value="Send" />
+      </form>
+    </Layout>
+  </>
+)
