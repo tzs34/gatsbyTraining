f989aaa787ef173dd9c57e504b16977513569315
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-09-26 20:53:41 +0100

    Remove form

--- src/pages/finance.js
+++ src/pages/finance.js
@@ -4,31 +4,6 @@ import GlobalStyle from '../components/styled/global-style'
 export default () => (
   <>
     <GlobalStyle />
-    <Layout>
-      <form>
-        <input
-          name="name"
-          placeholder="What is your name?"
-          class="name"
-          required
-        />
-        <input
-          name="emailaddress"
-          placeholder="What is your email?"
-          class="email"
-          type="email"
-          required
-        />
-        <textarea
-          rows="4"
-          cols="50"
-          name="subject"
-          placeholder="Please enter your message"
-          class="message"
-          required
-        ></textarea>
-        <input name="submit" class="btn" type="submit" value="Send" />
-      </form>
-    </Layout>
+    <Layout></Layout>
   </>
 )
