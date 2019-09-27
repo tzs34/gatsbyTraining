affdf6185f7f22000ddf557851b9088558bce07c
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-09-27 17:35:19 +0100

    Added dependecy gatsby-plugin-transitions for smooth page transitions

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,7 @@ module.exports = {
     'gatsby-image',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
+    'gatsby-plugin-transition-link',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
