db7576cc8630cf5e2c394cffc697853c26f13fc3
Dave Thorneycroft <dthorneycroft@goodprcatice.com>
2019-09-27 16:25:09 +0100

    Added available vehicles view to app

--- src/components/styled/components.js
+++ src/components/styled/components.js
@@ -1,5 +1,17 @@
 import styled from 'styled-components'
 
+const screenSize = {
+  mobile: '375',
+  tablet: '480px',
+  desktop: '768px',
+}
+
+export const device = {
+  mobile: `(min-width: ${screenSize.mobile})`,
+  tablet: `(min-width: ${screenSize.tablet})`,
+  desktop: `(min-width: ${screenSize.desktop})`,
+}
+
 export const Card = styled.div`
   width: 20%;
   min-width: 200px;
@@ -28,10 +40,54 @@ export const CardText = styled.div`
   padding: 5px;
   text-align: center;
 `
+export const GridColumn = styled.div`
+  --width: ${props => props.width};
+  order: ${props => props.moborder};
+  @media only screen and ${device.tablet} {
+    --width: ${({ mobile, tablet }) => tablet || mobile};
+    order: ${({ moborder, taborder }) => taborder || moborder};
+  }
+  @media only screen and ${device.desktop} {
+    --width: ${({ desktop, tablet }) => desktop || tablet};
+    order: ${({ dtorder, taborder }) => dtorder || taborder};
+  }
+`
 
-export const Row = styled.div`
+export const GridRow = styled.div`
   margin: 0;
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
+  --columns: ${props => props.columns || 4};
+  margin: 1rem;
+  ${GridColumn} {
+    --initialbasis: calc(var(--width, 0) / var(--columns) * 100%);
+    --gap: calc((var(--columns) - var(--width)) * 0.2%);
+    flex-basis: calc(var(--initialbasis) - var(--gap));
+  }
+`
+
+export const Row = styled.div`
+  margin: 0;
+  display: flex;
+  flex-flow: ${({ wrap }) => (wrap ? 'row wrap' : 'row nowrap')};
+  justify-content: space-between;
+`
+
+export const StickyContainer = styled.div`
+  ${({ isSticky }) =>
+    isSticky &&
+    `
+    position: fixed;
+    width: 100%;
+    z-index: 99;
+    bottom: 0px;
+  `};
+`
+
+export const Line = styled.div`
+  display: ${({ visible }) => (visible === true ? 'block' : 'none')};
+  border-bottom: 1px solid #dedede;
+  margin: 0;
+  width: 100%;
 `
--- src/pages/available-cars.js
+++ src/pages/available-cars.js
@@ -1,9 +1,51 @@
 import React from 'react'
 import Layout from '../components/layout/layout'
 import GlobalStyle from '../components/styled/global-style'
-export default () => (
-  <>
-    <GlobalStyle />
-    <Layout></Layout>
-  </>
-)
+import VehicleTile from '../components/vehicle/vehicle-tile'
+import { graphql } from 'gatsby'
+
+const AvailableVehicles = props => {
+  const {
+    data: {
+      cars: { edges: carData },
+    },
+  } = props
+  return (
+    <>
+      <GlobalStyle />
+      <Layout>
+        {props.data && (
+          <div>
+            {carData.map(({ node }, index) => (
+              <VehicleTile {...node} key={index} />
+            ))}
+          </div>
+        )}
+      </Layout>
+    </>
+  )
+}
+
+export const getCars = graphql`
+  query {
+    cars: allContentfulCars {
+      edges {
+        node {
+          model
+          manual
+          mileage
+          hybrid
+          image {
+            fluid(maxWidth: 500, maxHeight: 300) {
+              ...GatsbyContentfulFluid
+            }
+          }
+          efficient
+          owners
+        }
+      }
+    }
+  }
+`
+
+export default AvailableVehicles
